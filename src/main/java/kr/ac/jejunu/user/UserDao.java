1dc39c22ac3601002fea95b5199a9aa4a2232a04
고세종 <fulfilled7418@gmail.com>
2023-04-21 16:23:06 +0900

    Question 2

--- src/main/java/kr/ac/jejunu/user/UserDao.java
+++ src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -2,7 +2,7 @@ package kr.ac.jejunu.user;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public User findById(Long id) throws ClassNotFoundException, SQLException {
         Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select id, name, password from userinfo where id = ?");
@@ -38,9 +38,8 @@ public class UserDao {
 
     }
 
-    private static Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/portalservice", "root", "rootsejong");
-        return connection;
-    }
+    abstract public Connection getConnection() throws ClassNotFoundException, SQLException;
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/portalservice", "root", "rootsejong");
+//        return connection;
 }
--- src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -14,7 +14,7 @@ public class UserDaoTests {
     public void get() throws SQLException, ClassNotFoundException {
         Long id = 1l;
 
-        UserDao userDao = new UserDao();
+        UserDao userDao = new JejuUserDao();
         User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -27,7 +27,33 @@ public class UserDaoTests {
         user.setName(name);
         user.setPassword(password);
 
-        UserDao userDao = new UserDao();
+        UserDao userDao = new JejuUserDao();
+
+        userDao.insert(user);
+
+        User insertedUser = userDao.findById(user.getId());
+        assertThat(insertedUser.getName(), is(name));
+        assertThat(insertedUser.getPassword(), is(password));
+    }
+
+    @Test
+    public void getHalla() throws SQLException, ClassNotFoundException {
+        Long id = 1l;
+
+        UserDao userDao = new HallaUserDao();
+        User user = userDao.findById(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void insertHalla() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new HallaUserDao();
 
         userDao.insert(user);
 
